31a4df0e80806e8135cc5bcf06bbd8038a7a64f3
tmaulon <thomas.maulon@gmail.com>
2019-04-23 14:01:08 +0200

    Modifications : Order & OrderTest

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -5,13 +5,26 @@ public class Order {
      * Display all available menus in the restaurant.
      */
     public void displayAvailableMenu() {
-
+        System.out.println("Choix menu");
+        System.out.println("1 - poulet");
+        System.out.println("2 - boeuf");
+        System.out.println("3 - végétarien");
+        System.out.println("Que souhaitez-vous comme menu ?");
     }
     /**
      * Display a selected menu.
      * @param nbMenu The selected menu.
      */
     public void displaySelectedMenu(int nbMenu) {
+        if (nbMenu == 1) {
+            System.out.println("Vous avez choisi comme menu : poulet");
+        } else if (nbMenu == 2) {
+            System.out.println("Vous avez choisi comme menu : Boeuf");
+        } else if (nbMenu == 3) {
+            System.out.println("Vous avez choisi comme menu : Végétarien");
+        } else {
+            System.out.println("Vous n'avez pas choisi un des choix proposés comme menu");
+        }
 
     }
 }
